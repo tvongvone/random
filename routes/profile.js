6078b1856039aa05675067612f87075c008bff33
Tony Vongvone <tvongvone@gmail.com>
2025-02-12 22:14:03 -0800

    fetch profile

--- routes/profile.js
+++ routes/profile.js
@@ -5,3 +5,7 @@ const router = express.Router();
 router.get('/test', (_, res) => {
     res.send("This is a test")
 })
+
+router.get('/profile', (_, res) => {
+    res.send("This is your profile"); // This message is a placeholder
+})
