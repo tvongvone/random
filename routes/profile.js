e808b5a2dff5a14cd9d8a43b64cd2d9c35c8c691
Tony Vongvone <tvongvone@gmail.com>
2025-02-13 23:29:04 -0800

    login and signup

--- routes/profile.js
+++ routes/profile.js
@@ -9,3 +9,11 @@ router.get('/test', (_, res) => {
 router.get('/profile', (_, res) => {
     res.send("This is your profile"); // This message is a placeholder
 })
+
+router.post('/signup', (req, _) => {
+    res.send("Validated stuff")
+})
+
+router.post('/login', (req, _) => {
+    res.send("Successful login")
+})
